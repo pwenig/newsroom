65afdb0f5f7595930702c95d731b1bb0ddf063f7
Paul Wenig <pwenig@gmail.com>
2015-05-25 08:18:20 -0600

    Wired colletion into postsLists template

--- server/fixtures.js
+++ server/fixtures.js
@@ -7,7 +7,7 @@ if (Posts.find().count() === 0) { Posts.insert({
     title: 'Meteor',
     url: 'http://meteor.com'
   });
-  
+
   Posts.insert({
     title: 'The Meteor Book',
     url: 'http://themeteorbook.com'
